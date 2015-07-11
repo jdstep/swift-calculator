639783d7d966fe60c4257dca64994983356ba5ec
JD <jdstep23@gmail.com>
2015-07-10 19:53:09 -0700

    prepare to commit to github

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        println("ready to go to GitHub")
     }
 
     override func didReceiveMemoryWarning() {
