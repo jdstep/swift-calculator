f1c038a7eab8b72a303cb6735832caf90e772203
JD <jdstep23@gmail.com>
2015-07-11 16:18:37 -0700

    enter button stores display in a stack

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -11,22 +11,67 @@ import UIKit
 class ViewController: UIViewController {
 
 
-    @IBOutlet weak var numDisplay: UILabel!
-    
-    var userIsTyping: Bool = false
+  @IBOutlet weak var numDisplay: UILabel!
+  
+  
+  var userIsTyping: Bool = false
 
-    @IBAction func appendDigit(sender: UIButton) {
-        let digit = sender.currentTitle!
-        if (userIsTyping) {
-            println(digit)
-            numDisplay.text = numDisplay.text! + digit
-            
-        } else {
-            numDisplay.text = digit
-            userIsTyping = true
-        }
-        
-            
+
+  @IBAction func operatorButton(sender: UIButton) {
+    let operation = sender.currentTitle!
+    
+    switch operation {
+      
+    case "×": println("in switch with ×")
+      
+    default : println("nothing happened in switch")
+      
     }
+  
+  }
+  
+  func resetDisplayNum() {
+    numDisplay.text! = "0"
+  }
+  
+  
+    
+ 
+  var numStack = Array<Double>()
+  
+  // converts the display between double and string
+  var currentDisplayNum: Double {
+    get {
+      return NSNumberFormatter().numberFromString(numDisplay.text!)!.doubleValue
+    }
+    set {
+      numDisplay.text! = "\(newValue)"
+    }
+  }
+
+  
+  @IBAction func enter() {
+    numStack.append(currentDisplayNum)
+    userIsTyping = false
+    println(numStack)
+  }
+  
+  
+  
+  
+    
+    
+    
+  @IBAction func appendDigit(sender: UIButton) {
+    let digit = sender.currentTitle!
+    if (userIsTyping) {
+      numDisplay.text = numDisplay.text! + digit
+    } else {
+      numDisplay.text = digit
+      userIsTyping = true
+    }
+      
+      
+  }
 }
 
