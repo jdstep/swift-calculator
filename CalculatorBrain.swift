e3ceed97a394fc30933c59789867ffd52a97dc2b
JD <jdstep23@gmail.com>
2015-07-12 16:22:14 -0700

    calculator logic exists in brain model

--- CalculatorBrain.swift
+++ CalculatorBrain.swift
@@ -30,30 +30,67 @@ class CalculatorBrain {
     knownOps["cos"] = Op.UnaryOperation("cos", cos)
   }
   
+  // recursively performs the operations on the operands
   func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+    // if there are still remaining operands
     if !ops.isEmpty {
+      // store the remaining operands
       var remainingOps = ops
+      // get the top operand off the stack
       let op = remainingOps.removeLast()
+      // handle the different types of operands
       switch op {
-      case .Operand(let operand): return (operand, remainingOps)
+        // if the operand is a number
+      case .Operand(let operand):
+        // return the number along with the rest of the stack
+        return (operand, remainingOps)
+      // if the operand is a unary operation
       case .UnaryOperation(_, let operation):
+        // get the number next in the stack
         let operandEvaluation = evaluate(remainingOps)
+        // if the number value was present
         if let operand = operandEvaluation.result {
+          // return the result of performing the operation on that number
           return (operation(operand), operandEvaluation.remainingOps)
         }
+      // if the operand is a binary operation
       case .BinaryOperation(_, let operation):
+        // get the next number in the stack
         let operandEvalulation1 = evaluate(remainingOps)
+        // if that next number exists
         if let operand1 = operandEvalulation1.result {
+          // get the following number in the stack using the remainder
+          // after getting the previous number
           let operandEvaluation2 = evaluate(operandEvalulation1.remainingOps)
+          // if there is a second number
           if let operand2 = operandEvaluation2.result {
+            // perform the operation on the two numbers
             return (operation(operand1, operand2), operandEvaluation2.remainingOps)
           }
           
         }
       }
     }
+    // if any step failed, return nil
     return (nil, ops)
   }
+
+  func evaluateAll() -> Double? {
+    let (result, remainder) = evaluate(opStack)
+    println("\(result) is the result with \(remainder) left over")
+    return result
+  }
   
+  func pushOperand(operand: Double) -> Double? {
+    opStack.append(Op.Operand(operand))
+    return evaluateAll()
+  }
+  
+  func performOperation(symbol: String) -> Double? {
+    if let operation = knownOps[symbol] {
+      opStack.append(operation)
+    }
+    return evaluateAll()
+  }
     
 }
--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -15,42 +15,54 @@ class ViewController: UIViewController {
   
   
   var userIsTyping: Bool = false
+  
+  var brain = CalculatorBrain()
 
 
   @IBAction func operatorButton(sender: UIButton) {
     let operation = sender.currentTitle!
     
-    switch operation {
-      
-    case "×": performOperation() { $1 * $0 }
-    case "÷": performOperation() { $1 / $0 }
-    case "−": performOperation() { $1 - $0 }
-    case "+": performOperation() { $1 + $0 }
-    case "√": performOperationOneArg() { sqrt($0) }
-    case "sin": performOperationOneArg() { sin($0) }
-    case "cos": performOperationOneArg() { cos($0) }
+    if (userIsTyping) {
+      enter()
+    }
+    
+    if let operation = sender.currentTitle {
+      if let result = brain.performOperation(operation) {
+        currentDisplayNum = result
+      } else {
+        // need error handling here
+        currentDisplayNum = 0
+      }
+    }
+    
+    brain.performOperation(operation)
 
   
-    default : println("ERROR: no cases matched operation switch")
-      
-    }
-  
   }
   
-  // handle generic math functions with two arguments
-  func performOperation(operationFunc: (Double, Double) -> Double) {
-    if (numStack.count >= 2) {
-      currentDisplayNum = operationFunc(numStack.removeLast(), numStack.removeLast())
-      enter()
+  @IBAction func appendDigit(sender: UIButton) {
+    let digit = sender.currentTitle!
+    
+    // if the user tapped the decimal button
+    if (digit == ".") {
+      // if a decimal was already entered in the number
+      if (enteredDot) {
+        // eject from the function
+        return
+      } else {
+        // remember that we entered a decimal, and proceed with the rest of the function
+        enteredDot = true
+      }
     }
-  }
-  
-  // handle generic math functions with one argument
-  func performOperationOneArg(operationFunc: (Double) -> Double) {
-    if (numStack.count >= 1) {
-      currentDisplayNum = operationFunc(numStack.removeLast())
-      enter()
+    
+    if (userIsTyping) {
+      numDisplay.text = numDisplay.text! + digit
+    } else {
+      numDisplay.text = digit
+      userIsTyping = true
     }
+    
+    
   }
   
  
@@ -69,7 +81,12 @@ class ViewController: UIViewController {
 
   
   @IBAction func enter() {
-    numStack.append(currentDisplayNum)
+    if let result = brain.pushOperand(currentDisplayNum) {
+      currentDisplayNum = result
+    } else {
+      // should handle an error here
+      currentDisplayNum = 0
+    }
     userIsTyping = false
     enteredDot = false
   }
@@ -80,30 +97,7 @@ class ViewController: UIViewController {
     
     
     
-  @IBAction func appendDigit(sender: UIButton) {
-    let digit = sender.currentTitle!
-    
-    // if the user tapped the decimal button
-    if (digit == ".") {
-      // if a decimal was already entered in the number
-      if (enteredDot) {
-        // eject from the function
-        return
-      } else {
-        // remember that we entered a decimal, and proceed with the rest of the function
-        enteredDot = true
-      }
-    }
-
-    if (userIsTyping) {
-      numDisplay.text = numDisplay.text! + digit
-    } else {
-      numDisplay.text = digit
-      userIsTyping = true
-    }
-      
-      
-  }
+ 
   
   @IBAction func addConstant(sender: UIButton) {
     let mathConstant = sender.currentTitle!
