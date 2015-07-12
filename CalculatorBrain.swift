3cfe2c8bfd650317d6d840bb359a7015905b5c84
JD <jdstep23@gmail.com>
2015-07-12 15:56:50 -0700

    known ops and basic calculator evaluation logic

--- CalculatorBrain.swift
+++ CalculatorBrain.swift
@@ -30,8 +30,29 @@ class CalculatorBrain {
     knownOps["cos"] = Op.UnaryOperation("cos", cos)
   }
   
-  func evalate() {
-    
+  func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+    if !ops.isEmpty {
+      var remainingOps = ops
+      let op = remainingOps.removeLast()
+      switch op {
+      case .Operand(let operand): return (operand, remainingOps)
+      case .UnaryOperation(_, let operation):
+        let operandEvaluation = evaluate(remainingOps)
+        if let operand = operandEvaluation.result {
+          return (operation(operand), operandEvaluation.remainingOps)
+        }
+      case .BinaryOperation(_, let operation):
+        let operandEvalulation1 = evaluate(remainingOps)
+        if let operand1 = operandEvalulation1.result {
+          let operandEvaluation2 = evaluate(operandEvalulation1.remainingOps)
+          if let operand2 = operandEvaluation2.result {
+            return (operation(operand1, operand2), operandEvaluation2.remainingOps)
+          }
+          
+        }
+      }
+    }
+    return (nil, ops)
   }
   
     
